af056c3740b5e1444507f2934c201927a130675c
Prem <premcs.v@gmail.com>
2025-08-15 21:25:44 +0530

    firebase added

---
+++ src/app/FirebaseInit.tsx
@@ -0,0 +1,8 @@
+// src/app/FirebaseInit.tsx
+"use client";
+
+import "./utils/firebase"; // Just importing runs analytics
+
+export default function FirebaseInit() {
+  return null; // No UI, just side-effect
+}
---
+++ src/app/utils/firebase.js
@@ -0,0 +1,23 @@
+// src/app/utils/firebase.js
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAnalytics, logEvent  } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2xebai97FQTqgzuQIoOOQYyQsUpndLmo",
+  authDomain: "sarkariresult-gov.firebaseapp.com",
+  projectId: "sarkariresult-gov",
+  storageBucket: "sarkariresult-gov.firebasestorage.app",
+  messagingSenderId: "767950036220",
+  appId: "1:767950036220:web:09c71ff28d2c00af1d4b79",
+  measurementId: "G-0T6X74GEW8"
+};
+
+// Initialize Firebase only once
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+// Run analytics only in the browser
+if (typeof window !== "undefined") {
+  getAnalytics(app);
+}
+
+export { app };
